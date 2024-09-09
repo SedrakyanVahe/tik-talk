8a57967e2bd6b379e7001d75bf6d4cff5ab27857
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2024-09-09 12:36:57 +0400

    Added login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './common-ui/layout/layout.component';
+import { CanActivateAuth } from './auth/access.guard';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,8 @@ export const routes: Routes = [
       { path: '', component: SearchPageComponent },
       { path: 'profile', component: ProfilePageComponent },
     ],
+
+    canActivate: [CanActivateAuth],
   },
   { path: 'login', component: LoginPageComponent },
 ];
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { TokenResponse } from './auth.interface';
+import { tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  http = inject(HttpClient);
+  cookieService = inject(CookieService);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
+  token: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token');
+    }
+
+    return !!this.token;
+  }
+
+  login(payload: { username: string; password: string }) {
+    const formData = new FormData();
+    formData.append('username', payload.username);
+    formData.append('password', payload.password);
+
+    return this.http
+      .post<TokenResponse>(`${this.baseApiUrl}token`, formData)
+      .pipe(
+        tap((val) => {
+          this.token = val.access_token;
+          this.refreshToken = val.refresh_token;
+
+          this.cookieService.set('token', this.token);
+          this.cookieService.set('refreshToken', this.refreshToken);
+        })
+      );
+  }
+}
